50749efe9d6c4b30f3e6542333ec900fffee4433
jamesdanielwhitford <james.whitford0@gmail.com>
2022-09-19 13:58:49 +0300

    Add Update and Delete

--- my-app/app.js
+++ my-app/app.js
@@ -1,11 +1,15 @@
 import { Calendar } from './node_modules/@bryntum/calendar/calendar.module.js';
-// import { displayEvents } from './ui.js';
 
 const calendar = new Calendar({
 
     // append to the div with id="calendar"
     appendTo : document.getElementById('calendar'),
 
+    listeners : {
+        dataChange: function(event) {
+            updateMicrosoft(event);
+        }
+         },
     resources : [
         {
             id         : 1,
@@ -13,21 +17,49 @@ const calendar = new Calendar({
             eventColor : 'green'
         }
     ],
-    events : [
-        {
-            id         : 1,
-            name       : 'Meeting',
-            startDate  : '2022-09-11T10:00:00',
-            endDate    : '2022-09-11T11:00:00',
-            resourceId : 1
-        }
-    ]
 });
 
-document.querySelector('#signin').addEventListener('click', displayUI);
-// document.querySelector('#btnShowEvents').addEventListener('click', displayEvents);
-export { calendar };
+async function updateMicrosoft(event) {
+    if(event.action == "update") {
+        const microEvents = await getAllEvents();
+        // check if event exists in microsoft, if it does, update it, if not, create it
+        var eventExists = false;
 
+        microEvents.value.forEach(microEvent => {
+            // event exists in both microsoft and bryntum with the same name
+            if(microEvent.subject == event.record.name) {
+                eventExists = true;
+                updateEvent(microEvent.id, event.record.name, event.record.startDate, event.record.endDate);
+            } 
+            // The event exists but the name was updated
+            else if('duration' in event.changes || 'name' in event.changes) {
+                if ('name' in event.changes) {
+                    if (event.changes.name.oldValue != "New event"){
+                        eventExists = true;
+                        updateEvent(microEvent.id, event.record.name, event.record.startDate, event.record.endDate);
+                    }
+            }
+        }
+        });
+        // event does not exist in microsoft, create it
+        if(!eventExists) {
+            if (event.record.name != undefined) {
+                createEvent(event.record.name, event.record.startDate, event.record.endDate);
+            } 
+        }
+
+    }
+    // event is deleted
+    else if (event.action == "remove") {
+        const microEvents = await getAllEvents();
+        var eventName = event.records[0].data.name;
+        microEvents.value.forEach(event => {
+            if (event.subject == eventName) {
+                deleteEvent(event.id);
+            }
+        });
+    }   
+}
 
 async function displayUI() {    
     await signIn();
@@ -39,7 +71,7 @@ async function displayUI() {
     content.style = "display: block";
 
     // Display calendar after login
-    var events = await getEvents();
+    var events = await getNextWeeksEvents();
     var calendarEvents = [];
     var eventId = 1;
     var resourceID = 1;
@@ -58,4 +90,7 @@ async function displayUI() {
 
 }
 
+document.querySelector('#signin').addEventListener('click', displayUI);
+
+export { calendar }
 export { displayUI };
